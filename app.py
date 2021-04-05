6990d84ffe4ae227f8921be85ae6810b594f50f1
irasan <iryna.sanzhara@gmail.com>
2021-04-05 19:55:42 +0000

    Add register functionality

--- app.py
+++ app.py
@@ -26,6 +26,25 @@ def home():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        if (request.form.get("password") == request.form.get("password1")):
+            register = {
+                "username": request.form.get("username").lower(),
+                "password": generate_password_hash(request.form.get("password"))
+            }
+            mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("register.html")
 
 
