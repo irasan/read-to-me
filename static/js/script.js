1a58d2735e1a81e927231060e3c8ab69076cf808
irasan <iryna.sanzhara@gmail.com>
2021-04-27 10:24:03 +0000

    Add confirm message for delete buttons

--- static/js/script.js
+++ static/js/script.js
@@ -46,3 +46,7 @@ document.getElementById("author").addEventListener('keydown', function (e) {
 document.getElementById("isbn").addEventListener('keydown', function (e) {
   if (this.value.length === 0 && e.which === 32) e.preventDefault();
 });
+
+document.getElementById("query").addEventListener('keydown', function (e) {
+  if (this.value.length === 0 && e.which === 32) e.preventDefault();
+});
